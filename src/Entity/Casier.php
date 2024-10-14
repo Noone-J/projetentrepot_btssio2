f3cacdf96e901dc5537d165bd52dc4bdf35f3b03
Elladan <elladan.serrano@gmail.com>
2024-10-14 15:29:11 +0200

    commit_2

--- src/Entity/Casier.php
+++ src/Entity/Casier.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CasierRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CasierRepository::class)]
@@ -18,6 +20,20 @@ class Casier
 
     #[ORM\Column]
     private ?int $taille = null;
+
+    #[ORM\ManyToOne(inversedBy: 'lesCasiers')]
+    private ?Entrepot $lEntrepot = null;
+
+    /**
+     * @var Collection<int, Compartiment>
+     */
+    #[ORM\OneToMany(targetEntity: Compartiment::class, mappedBy: 'leCasier')]
+    private Collection $lesCompartiments;
+
+    public function __construct()
+    {
+        $this->lesCompartiments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +60,48 @@ class Casier
     public function setTaille(int $taille): static
     {
         $this->taille = $taille;
+
+        return $this;
+    }
+
+    public function getLEntrepot(): ?Entrepot
+    {
+        return $this->lEntrepot;
+    }
+
+    public function setLEntrepot(?Entrepot $lEntrepot): static
+    {
+        $this->lEntrepot = $lEntrepot;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Compartiment>
+     */
+    public function getLesCompartiments(): Collection
+    {
+        return $this->lesCompartiments;
+    }
+
+    public function addLesCompartiment(Compartiment $lesCompartiment): static
+    {
+        if (!$this->lesCompartiments->contains($lesCompartiment)) {
+            $this->lesCompartiments->add($lesCompartiment);
+            $lesCompartiment->setLeCasier($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLesCompartiment(Compartiment $lesCompartiment): static
+    {
+        if ($this->lesCompartiments->removeElement($lesCompartiment)) {
+            // set the owning side to null (unless already changed)
+            if ($lesCompartiment->getLeCasier() === $this) {
+                $lesCompartiment->setLeCasier(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Entrepot.php
+++ src/Entity/Entrepot.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\EntrepotRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EntrepotRepository::class)]
@@ -21,6 +23,24 @@ class Entrepot
 
     #[ORM\Column]
     private ?bool $status = null;
+
+    /**
+     * @var Collection<int, Casier>
+     */
+    #[ORM\OneToMany(targetEntity: Casier::class, mappedBy: 'lEntrepot')]
+    private Collection $lesCasiers;
+
+    /**
+     * @var Collection<int, Distance>
+     */
+    #[ORM\OneToMany(targetEntity: Distance::class, mappedBy: 'lEntrepot')]
+    private Collection $lesDistances;
+
+    public function __construct()
+    {
+        $this->lesCasiers = new ArrayCollection();
+        $this->lesDistances = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +79,66 @@ class Entrepot
     public function setStatus(bool $status): static
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Casier>
+     */
+    public function getLesCasiers(): Collection
+    {
+        return $this->lesCasiers;
+    }
+
+    public function addLesCasier(Casier $lesCasier): static
+    {
+        if (!$this->lesCasiers->contains($lesCasier)) {
+            $this->lesCasiers->add($lesCasier);
+            $lesCasier->setLEntrepot($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLesCasier(Casier $lesCasier): static
+    {
+        if ($this->lesCasiers->removeElement($lesCasier)) {
+            // set the owning side to null (unless already changed)
+            if ($lesCasier->getLEntrepot() === $this) {
+                $lesCasier->setLEntrepot(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Distance>
+     */
+    public function getLesDistances(): Collection
+    {
+        return $this->lesDistances;
+    }
+
+    public function addLesDistance(Distance $lesDistance): static
+    {
+        if (!$this->lesDistances->contains($lesDistance)) {
+            $this->lesDistances->add($lesDistance);
+            $lesDistance->setLEntrepot($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLesDistance(Distance $lesDistance): static
+    {
+        if ($this->lesDistances->removeElement($lesDistance)) {
+            // set the owning side to null (unless already changed)
+            if ($lesDistance->getLEntrepot() === $this) {
+                $lesDistance->setLEntrepot(null);
+            }
+        }
 
         return $this;
     }
